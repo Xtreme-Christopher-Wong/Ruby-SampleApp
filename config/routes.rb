3aca7b5f2cc769ee08230babd922417771b12f52
Chris Wong <cwong@pivotal.io>
2017-06-06 16:50:19 +0000

    Add Static Pages, Schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
-
   root 'application#hello'
 end
