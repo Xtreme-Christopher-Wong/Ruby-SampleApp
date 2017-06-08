77cb37c8297d0a336f51af440d040f78da9e1f00
Chris Wong <cwong@pivotal.io>
2017-06-08 15:08:47 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-  root 'application#hello'
+  get 'static_pages/about'
+  
+  #Exercise from 3.4.3
+  get 'static_pages/contact'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,15 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  
+require 'minitest/reporters'
+
+# Fix the "minitest_plugin.rb:9:in" error
+# >https://stackoverflow.com/questions/43929510/michael-hartl-chapter-3-3-1-rails-test-error-messag
+# >>http://motion-express.com/blog/minitest-setup-for-rails
+Minitest::Reporters.use!(
+  Minitest::Reporters::ProgressReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
 end
